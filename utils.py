5d69455542047013e1a42c44e240d4a4ea0d1933
clpy9793 <wqs1639@gmail.com>
2017-06-05 09:26:04 +0800

    .

--- utils.py
+++ utils.py
@@ -24,6 +24,28 @@ try:
 except ImportError:
     from urllib import unquote
 
+def patch_crypto_be_discovery():
+    """
+    Monkey patches cryptography's backend detection.
+    Objective: support pyinstaller freezing.
+    """
+    from cryptography.hazmat import backends
+
+    try:
+        from cryptography.hazmat.backends.commoncrypto.backend import backend as be_cc
+    except ImportError:
+        be_cc = None
+
+    try:
+        from cryptography.hazmat.backends.openssl.backend import backend as be_ossl
+    except ImportError:
+        be_ossl = None
+
+    backends._available_backends_list = [
+        be for be in (be_cc, be_ossl) if be is not None
+    ]
+
+
 @try_again(3)
 def q():
     import random
